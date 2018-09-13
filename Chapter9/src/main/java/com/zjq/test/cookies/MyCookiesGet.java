912bc3002e9bf25761e7ca635ed759f64bb422d7
bjzhaojianqiang <bjzhaojianqiang@corp.netease.com>
2018-09-13 20:45:54 +0800

    第七章

--- Chapter9/src/main/java/com/zjq/test/cookies/MyCookiesGet.java
+++ Chapter9/src/main/java/com/zjq/test/cookies/MyCookiesGet.java
@@ -1,14 +1,18 @@
 package com.zjq.test.cookies;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -16,6 +20,8 @@ public class MyCookiesGet {
 
     private String url;
     private ResourceBundle bundle;
+
+    private CookieStore cookieStore;
 
     @BeforeTest
     public void beforeTest(){
@@ -26,9 +32,34 @@ public class MyCookiesGet {
     public void testGetCookies() throws IOException {
         String result;
         HttpGet get = new HttpGet(this.url + bundle.getString("getCookies.uri"));
-        HttpClient client = HttpClientBuilder.create().build();;
+        DefaultHttpClient client = new DefaultHttpClient();
         HttpResponse response = client.execute(get);
-        result = EntityUtils.toString(response.getEntity(),"UTF-8");
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+
         System.out.println(result);
+        this.cookieStore = client.getCookieStore();
+        List<Cookie> cookieList = this.cookieStore.getCookies();
+        for (Cookie cookie : cookieList){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie name = " + name
+                    + ";  cookie value = " + value);
+        }
+    }
+
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testGetWithCookies() throws IOException {
+        String result;
+        HttpGet get = new HttpGet(this.url + bundle.getString("test.get.with.cookies"));
+        DefaultHttpClient client = new DefaultHttpClient();
+        //设置cookie信息
+        client.setCookieStore(this.cookieStore);
+        HttpResponse response = client.execute(get);
+        int statusCode =  response.getStatusLine().getStatusCode();
+        System.out.println("statusCode:" + statusCode);
+        if (statusCode ==200){
+            result = EntityUtils.toString(response.getEntity(),"utf-8");
+            System.out.println(result);
+        }
     }
 }

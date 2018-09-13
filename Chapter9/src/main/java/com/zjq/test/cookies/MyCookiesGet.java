108b964b5332d7d2f89e75884fc7788ab5cea36c
zjqifm19931110 <13263397628@126.com>
2018-09-13 21:59:35 +0800

    第9章

--- Chapter9/src/main/java/com/zjq/test/cookies/MyCookiesGet.java
+++ Chapter9/src/main/java/com/zjq/test/cookies/MyCookiesGet.java
@@ -1,14 +1,18 @@
 package com.zjq.test.cookies;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -26,9 +30,18 @@ public class MyCookiesGet {
     public void testGetCookies() throws IOException {
         String result;
         HttpGet get = new HttpGet(this.url + bundle.getString("getCookies.uri"));
-        HttpClient client = HttpClientBuilder.create().build();;
+        DefaultHttpClient client = new DefaultHttpClient();
         HttpResponse response = client.execute(get);
         result = EntityUtils.toString(response.getEntity(),"UTF-8");
-        System.out.println(result);
+
+        CookieStore store = client.getCookieStore();
+        List<Cookie> cookies = store.getCookies();
+        System.out.println(cookies);
+        for (Cookie cookie: cookies) {
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println(name + ":" + value);
+
+        }
     }
 }
